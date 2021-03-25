86584b56eede50e06093ee5a81091f1a0a5f0bed
ssukim <tntntn0930@gmail.com>
2021-03-25 15:47:28 +0900

    prisma architecture using graphql tools for file merge

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-import { ApolloServer, gql } from "apollo-server";
-import { resolvers, typeDefs } from "./schema";
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema
 });
 
 server.listen().then(() => console.log("Server is running on http://localhost:4000"))
