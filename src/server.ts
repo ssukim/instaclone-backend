3947d3c28edd35bc64912ba97f798e31d5a7ca70
ssukim <tntntn0930@gmail.com>
2021-04-06 11:07:04 +0900

    ejecting from apollo-server-express

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 require("dotenv").config();
-
-import { ApolloServer } from "apollo-server";
+import * as express from "express";
+import * as logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import client from "./client";
-import {typeDefs, resolvers} from "./schema";
+import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({
   resolvers,
   typeDefs,
@@ -16,8 +18,9 @@ const server = new ApolloServer({
   },
 });
 
-const PORT = process.env.PORT;
-
-server
-  .listen(PORT)
-  .then(() => console.log(`👍 Server is running on http://localhost:${PORT}/`));
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+  console.log(`👍 Server is running on http://localhost:${PORT}/graphql`);
+});
