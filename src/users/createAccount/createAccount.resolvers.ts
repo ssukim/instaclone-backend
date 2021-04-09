40bfd3022cb523245d1fbb4853fd7ea1f684d7ac
ssukim <tntntn0930@gmail.com>
2021-04-09 15:51:57 +0900

    Add seeFolloers Query

--- src/users/createAccount/createAccount.resolvers.ts
+++ src/users/createAccount/createAccount.resolvers.ts
@@ -1,13 +1,12 @@
 import * as bcrypt from "bcrypt";
 import client from "../../client";
+import { Resolvers } from "../../types";
+import { protectedResolver } from "../users.utils";
 
-export default {
+const resolvers: Resolvers = {
   Mutation: {
-    createAccount: async (
-      _,
-      { firstName, lastName, email, password, username }
-    ) => {
-      try {
+    createAccount: protectedResolver(
+      async (_, { firstName, lastName, email, password, username }) => {
         // check if username or email are already on DB
         const existingUser = await client.user.findFirst({
           where: {
@@ -21,15 +20,19 @@ export default {
             ],
           },
         });
+
         if (existingUser) {
-          throw new Error("This username/password is already taken.");
+          return {
+            ok: false,
+            error: "This username/password is already taken.",
+          };
         }
 
         // hash password
         const uglyPassword = await bcrypt.hash(password, 10);
 
         // save and return the user
-        return client.user.create({
+        await client.user.create({
           data: {
             username,
             email,
@@ -38,9 +41,13 @@ export default {
             password: uglyPassword,
           },
         });
-      } catch (e) {
-        console.log(e);
+
+        return {
+          ok: true,
+        };
       }
-    },
+    ),
   },
 };
+
+export default resolvers;
---
+++ src/users/seeFollowers/seeFollowers.resolvers.ts
@@ -0,0 +1,15 @@
+import client from "../../client";
+import { Resolvers } from "../../types";
+
+const resolvers: Resolvers = {
+  Query: {
+    seeFollowers: async (_, { username, page }) => {
+      const followers = await client.user
+        .findUnique({ where: { username } })
+        .followers({ take: 5, skip: (page - 1) * 5 });
+      return { ok: true, followers };
+    },
+  },
+};
+
+export default resolvers;
