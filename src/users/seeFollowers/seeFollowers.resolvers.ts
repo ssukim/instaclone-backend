167fcebce0f178337089ae6f4aeab0aeb350e86e
ssukim <tntntn0930@gmail.com>
2021-04-15 10:03:53 +0900

    seeFollowers resolvers totalPages 추가, username 예외처리 추가

--- src/users/seeFollowers/seeFollowers.resolvers.ts
+++ src/users/seeFollowers/seeFollowers.resolvers.ts
@@ -4,10 +4,23 @@ import { Resolvers } from "../../types";
 const resolvers: Resolvers = {
   Query: {
     seeFollowers: async (_, { username, page }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
       const followers = await client.user
         .findUnique({ where: { username } })
         .followers({ take: 5, skip: (page - 1) * 5 });
-      return { ok: true, followers };
+      const totalFollwers = await client.user.count({
+        where: { following: { some: { username } } },
+      });
+      return { ok: true, followers, totalPages: Math.ceil(totalFollwers / 5) };
     },
   },
 };
