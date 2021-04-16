cc18df03e0b8b193a407a7078701fdc768e969ac
ssukim <tntntn0930@gmail.com>
2021-04-16 11:46:20 +0900

    Computed Fileds isFollowing

--- src/users/users.resolvers.ts
+++ src/users/users.resolvers.ts
@@ -29,6 +29,22 @@ const resolvers: Resolvers = {
       }
       return id === loggedInUser.id;
     },
+    isFollowing: async ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      const exists = await client.user.count({
+        where: {
+          username: loggedInUser.username,
+          following: {
+            some: {
+              id,
+            },
+          },
+        },
+      });
+      return Boolean(exists);
+    },
   },
 };
 
